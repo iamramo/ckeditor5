4750e39e4a4befc577924d4155a2cb0f0d832ef7
Ramo Mislimi <ramo.mislimi@consid.se>
2020-10-25 19:39:51 +0100

    Removed unused plugins.

--- packages/ckeditor5-build-classic/src/ckeditor.js
+++ packages/ckeditor5-build-classic/src/ckeditor.js
@@ -7,13 +7,10 @@
 import ClassicEditorBase from '@ckeditor/ckeditor5-editor-classic/src/classiceditor';
 
 import Essentials from '@ckeditor/ckeditor5-essentials/src/essentials';
-import UploadAdapter from '@ckeditor/ckeditor5-adapter-ckfinder/src/uploadadapter';
 import Autoformat from '@ckeditor/ckeditor5-autoformat/src/autoformat';
 import Bold from '@ckeditor/ckeditor5-basic-styles/src/bold';
 import Italic from '@ckeditor/ckeditor5-basic-styles/src/italic';
 import BlockQuote from '@ckeditor/ckeditor5-block-quote/src/blockquote';
-import CKFinder from '@ckeditor/ckeditor5-ckfinder/src/ckfinder';
-import EasyImage from '@ckeditor/ckeditor5-easy-image/src/easyimage';
 import Heading from '@ckeditor/ckeditor5-heading/src/heading';
 import Image from '@ckeditor/ckeditor5-image/src/image';
 import ImageCaption from '@ckeditor/ckeditor5-image/src/imagecaption';
@@ -38,6 +35,8 @@ import IndentBlock from '@ckeditor/ckeditor5-indent/src/indentblock';
 import Underline from '@ckeditor/ckeditor5-basic-styles/src/underline';
 import Strikethrough from '@ckeditor/ckeditor5-basic-styles/src/strikethrough';
 import RemoveFormat from '@ckeditor/ckeditor5-remove-format/src/removeformat';
+// import WordCount from '@ckeditor/ckeditor5-word-count/src/wordcount';
+// import SimpleUploadAdapter from '@ckeditor/ckeditor5-upload/src/adapters/simpleuploadadapter';
 
 import './custom.css';
 
@@ -46,13 +45,10 @@ export default class ClassicEditor extends ClassicEditorBase {}
 // Plugins to include in the build.
 ClassicEditor.builtinPlugins = [
 	Essentials,
-	UploadAdapter,
 	Autoformat,
 	Bold,
 	Italic,
 	BlockQuote,
-	CKFinder,
-	EasyImage,
 	Heading,
 	Image,
 	ImageCaption,
@@ -75,7 +71,9 @@ ClassicEditor.builtinPlugins = [
 	IndentBlock,
 	Underline,
 	Strikethrough,
-	RemoveFormat
+	RemoveFormat,
+	// WordCount,
+	// SimpleUploadAdapter
 ];
 
 // Editor configuration.
